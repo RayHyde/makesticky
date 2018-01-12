5d293dea37e79f5029055ddd04a2d070b1d10ca2
Ray Hyde <ray@rayhyde.nl>
2018-01-12 09:04:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h1>Shooting ducks - a classic fairground favourite</h1>
+<h2>A simple, silly game — yet it increases your conversion!</h2>
+<p>Include this game in your webshop and have your customers win prizes, such as different discount levels or free products.</p>
+<p>The game has been made with CSS3 and jQuery. Mainly, the clicks and counters are jQuery while the animations are pure CSS. When speed is involved, it has built-in variables to easily adjust speed or difficulty level so you won't lose customers. But even then, hey, a booby prize can still be worth a product sample or free shipping!</p>
+<p>This game is licensed under GNU General Public License v2.0. That means you may use it commercially. However, I would like to be notified if you use this game and remember, it needs tweaking to your specific needs and that is where I might be of service.</p>
+<p>See more silly games like these at <a href="http://www.rayhyde.nl/playground/simple_games.html">my website</a>.</p>
+<div id="explain">
+<p>This project is part of my Playground - a collection of fun (and dare I say it: clever) stuff I made in the past, from jQuery games and plugins to CSS animation tricks.</p>
+<p>Please drop in on my portfolio site <br><a href="http://www.rayhyde.nl">www.rayhyde.nl</a>!</p>
+</div>
---
+++ js/jquery-makesticky.js
@@ -0,0 +1,67 @@
+(function ($) {
+		$.fn.makeSticky = function (options) {
+
+			var settings = $.extend({
+				stickBelow: $('header'),
+				oneWay: false,
+				shadow: true
+			}, options);
+
+			var $el = $(this),
+					h = settings.stickBelow.height(),
+					didScroll,
+					lastScrollTop = 0;
+
+			var makeSticky = function() {
+				$('body').addClass('make-sticky-active');
+				$el.addClass('active');
+				$('.make-sticky-wrapper').css({
+					top: h
+				}).addClass('active');
+			}
+			var removeSticky = function() {
+				$('body').removeClass('make-sticky-active');
+				$el.removeClass('active');
+				$('.make-sticky-wrapper').removeClass('active').css({top: 'auto'});
+			}
+
+			if (settings.shadow == true) {
+				$el.wrap('<div class="make-sticky-wrapper shadow"><div class="container">');
+			} else {
+				$el.wrap('<div class="make-sticky-wrapper"><div class="container">');
+
+			}
+			$(window).scroll(function (event) {
+				didScroll = true;
+			});
+			setInterval(function () {
+				if (didScroll) {
+					hasScrolled();
+					didScroll = false;
+				}
+			}, 250);
+			var hasScrolled = function() {
+				var st = $(this).scrollTop();
+
+				if ( settings.oneWay ) {
+					if (st > lastScrollTop && st > h || st < h ) {
+						// Scroll Down
+						removeSticky();
+					} else {
+						// Scroll Up
+						if (st + $(window).height() < $(document).height()) {
+							makeSticky();
+						}
+					}
+					lastScrollTop = st;
+				} else {
+					if ( st > h) {
+						makeSticky();
+					} else {
+						removeSticky();
+					}
+				}
+
+			}
+		}
+}(jQuery));
